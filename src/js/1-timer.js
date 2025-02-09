04bf040cd89218b7b2838775a129686595ac2146
Evelina Olshanska <olshanskae@gmail.com>
2025-02-09 20:29:11 +0200

    homework 10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,91 @@
-console.log('Timer');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  datePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysElem: document.querySelector('[data-days]'),
+  hoursElem: document.querySelector('[data-hours]'),
+  minutesElem: document.querySelector('[data-minutes]'),
+  secondsElem: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+
+let userSelectedDate = null;
+let countdownInterval = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      refs.startBtn.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      refs.startBtn.disabled = false;
+    }
+    // console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.datePicker, options);
+
+function startTimer() {
+  refs.startBtn.disabled = true;
+  refs.datePicker.disabled = true;
+
+  countdownInterval = setInterval(() => {
+    const currentTime = new Date();
+    const timeLeft = userSelectedDate - currentTime;
+
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay(0, 0, 0, 0);
+      refs.datePicker.disabled = false;
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    updateTimerDisplay(days, hours, minutes, seconds);
+  }, 1000);
+}
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  refs.daysElem.textContent = addLeadingZero(days);
+  refs.hoursElem.textContent = addLeadingZero(hours);
+  refs.minutesElem.textContent = addLeadingZero(minutes);
+  refs.secondsElem.textContent = addLeadingZero(seconds);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+refs.startBtn.addEventListener('click', startTimer);
+
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,46 @@
-console.log('snackbar');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delayInput: document.querySelector("input[name='delay']"),
+  stateInputs: document.querySelectorAll("input[name='state']"),
+};
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const delay = Number(refs.delayInput.value); 
+
+const selectedInput = [...refs.stateInputs].find(input => input.checked);
+const selectedState = selectedInput && selectedInput.value;
+
+
+  if (!selectedState) return;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      selectedState === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+}
+
+refs.form.addEventListener('submit', handleSubmit);
